134d3e63a1e81e319d4d56e081b5c7af1a2da039
Frederic Guillot <fred@kanboard.net>
2016-01-29 21:24:19 -0500

    Add travis-ci config

--- Plugin.php
+++ Plugin.php
@@ -11,7 +11,7 @@ class Plugin extends Base
     {
         $container = $this->container;
 
-        $this->hook->on('controller:calendar:user:events', function($user_id, $start, $end) use ($container) {
+        $this->hook->on('controller:calendar:user:events', function ($user_id, $start, $end) use ($container) {
             $model = new SubtaskForecast($container);
             return $model->getCalendarEvents($user_id, $end);
         });
